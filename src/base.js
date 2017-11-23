60b83fb09c520ad0c6db049df8f46698e3602667
Bobylove <delon.frederic.pro.job@gmail.com>
2017-11-23 17:10:32 +0100

    Initial commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base =  Rebase.createClass({
+	apiKey: "AIzaSyChbRAjZX20TdmphsPbzvKrRri-CwMfJ6Y",
+    authDomain: "ma-chat-box-boby.firebaseapp.com",
+    databaseURL: "https://ma-chat-box-boby.firebaseio.com"
+});
+
+export default base;
